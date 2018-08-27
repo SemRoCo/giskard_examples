18fe639083db64da2bf39a09d02c2d29d563fff5
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2018-08-27 09:42:21 +0200

    Added an option to publish Twist messages.

--- src/giskard_ros/joint_state_separator.cpp
+++ src/giskard_ros/joint_state_separator.cpp
@@ -26,20 +26,39 @@
 #include <std_msgs/Float64.h>
 #include <giskard_ros/ros_utils.hpp>
 
+#include <urdf/model.h>
+#include <geometry_msgs/Twist.h>
+
 namespace giskard_ros
 {
   class JointStateSeparator
   {
     public:
-      JointStateSeparator(const ros::NodeHandle& nh) : nh_(nh) {}
+      JointStateSeparator(const ros::NodeHandle& nh) : nh_(nh), robot_model_loaded_(false), output_twist_(false) {}
       ~JointStateSeparator() {}
 
       void start() 
       {
+        std::string robot_description_parameter = readParam<std::string>(nh_, "robot_description_parameter_name");
+        output_twist_ = readParam<bool>(nh_, "output_twist");
+        if (output_twist_ && !robot_model_.initParam(robot_description_parameter))
+        {
+            throw std::runtime_error("Could not read urdf from parameter server at '" + robot_description_parameter + "'.");
+        }
+
         joint_names_ = readParam< std::vector<std::string> >(nh_, "joint_names");
 
         for (size_t i=0; i<joint_names_.size(); ++i)
-          pubs_.push_back(nh_.advertise<std_msgs::Float64>("/" + joint_names_[i].substr(0, joint_names_[i].size() - 6) + "_velocity_controller/command", 1));
+        {
+            if(output_twist_)
+            {
+                pubs_.push_back(nh_.advertise<geometry_msgs::Twist>("/" + joint_names_[i].substr(0, joint_names_[i].size() - 6) + "_velocity_controller/command", 1));
+            }
+            else
+            {
+                pubs_.push_back(nh_.advertise<std_msgs::Float64>("/" + joint_names_[i].substr(0, joint_names_[i].size() - 6) + "_velocity_controller/command", 1));
+            }
+        }
 
         sub_ = nh_.subscribe("joint_states", 1, &JointStateSeparator::callback, this);
       }
@@ -49,6 +68,10 @@ namespace giskard_ros
       ros::Subscriber sub_;
       std::vector<ros::Publisher> pubs_;
       std::vector<std::string> joint_names_;
+
+      urdf::Model robot_model_;
+      bool robot_model_loaded_;
+      bool output_twist_;
 
       void callback(const sensor_msgs::JointState::ConstPtr& msg)
       {
@@ -67,9 +90,39 @@ namespace giskard_ros
           if (name_cmd_map.find(joint_names_[i]) == name_cmd_map.end())
             throw std::runtime_error("Could not find velocity command for joint '" + joint_names_[i] + "'.");
 
-          std_msgs::Float64 out_msg;
-          out_msg.data = name_cmd_map.find(joint_names_[i])->second;
-          pubs_[i].publish(out_msg);
+          if(output_twist_)
+          {
+            double vel = name_cmd_map.find(joint_names_[i])->second;
+            if (robot_model_.joints_.find(joint_names_[i]) == robot_model_.joints_.end() )
+                throw std::runtime_error("Could not find link with name '" + joint_names_[i] + "'.");
+            urdf::JointConstSharedPtr joint = robot_model_.joints_.find(joint_names_[i])->second;
+            bool prismatic = (joint->type == urdf::Joint::PRISMATIC);
+            bool revolute = ((joint->type == urdf::Joint::REVOLUTE) || (joint->type == urdf::Joint::CONTINUOUS));
+
+            geometry_msgs::Twist out_msg;
+            out_msg.linear.x = out_msg.linear.y = out_msg.linear.z = 0.0;
+            out_msg.angular.x = out_msg.angular.y = out_msg.angular.z = 0.0;
+            if(prismatic)
+            {
+                out_msg.linear.x = joint->axis.x*vel;
+                out_msg.linear.y = joint->axis.y*vel;
+                out_msg.linear.z = joint->axis.z*vel;
+            }
+            if(revolute)
+            {
+                out_msg.angular.x = joint->axis.x*vel;
+                out_msg.angular.y = joint->axis.y*vel;
+                out_msg.angular.z = joint->axis.z*vel;
+            }
+            pubs_[i].publish(out_msg);
+
+          }
+          else
+          {
+              std_msgs::Float64 out_msg;
+              out_msg.data = name_cmd_map.find(joint_names_[i])->second;
+              pubs_[i].publish(out_msg);
+          }
         }
       }
   };
